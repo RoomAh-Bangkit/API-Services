ce8204363f13f745111315b8b16849da438e52e3
alexanderpratama <101419838+alexanderpratama@users.noreply.github.com>
2022-06-12 00:28:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/loadimg', methods=['GET', 'POST'])
 def roomah():
     if request.method == 'POST':
     # POST method to post the results file
@@ -13,7 +13,8 @@ def roomah():
         # Get category of prediction
         image_category = get_category(img)
         # Plot the category
-        return make_response(jsonify(image_category))
+        return make_response(jsonify({"error":"false","message":"success","result":image_>
+    return make_response(jsonify({"error":"true","message":"failed"}))
 
 if __name__ == '__main__':
     app.run(debug=True)
